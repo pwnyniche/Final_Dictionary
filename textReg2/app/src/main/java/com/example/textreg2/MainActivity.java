3dc396acf42982896082c27485092ad4206e167c
Trần Quang Tiến <tqtnk2000@gmail.com>
2020-11-29 20:52:16 +0700

    add info

--- textReg2/app/src/main/java/com/example/textreg2/MainActivity.java
+++ textReg2/app/src/main/java/com/example/textreg2/MainActivity.java
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.icu.text.IDNA;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     CardView textReg;
     CardView dictionary;
+    CardView aboutUs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textReg.setOnClickListener(this);
         dictionary=findViewById(R.id.wordSearch);
         dictionary.setOnClickListener(this);
+        aboutUs=findViewById(R.id.aboutUs);
+        aboutUs.setOnClickListener(this);
     }
 
     @Override
@@ -27,9 +31,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent intent = new Intent(this,textRegActivity.class);
             startActivity(intent);
         }
-        if (v.getId()==R.id.wordSearch)
+        if (v.getId()==R.id.aboutUs)
         {
-
+            Intent infoIntent=new Intent(this, InfoActivity.class);
+            startActivity(infoIntent);
         }
     }
 }
